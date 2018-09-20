f250d4a111f9fda512780e5caf0223bc850cc69d
l0sted <losted@wants.dicksinhisan.us>
2018-09-20 22:57:37 +0300

    calculator

--- VazhovIA/lab1/calculator/main.cpp
+++ VazhovIA/lab1/calculator/main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    float left, right;
+    char sign;
+    cin >> left >> sign >> right;
+
+    switch (sign)
+    {
+    case '+':
+        cout << left + right << endl;
+        break;
+    case '-':
+        cout << left - right << endl;
+        break;
+    case '*':
+        cout << left * right << endl;
+        break;
+    case '/':
+        if (right == 0) {
+            cout << "Division by zero is forbidden" << endl;
+            return 8;
+        }
+        cout << left / right << endl;
+        break;
+    }
     return 0;
 }
