8751c3e0d77494ede6b580d9cf9fde7b64a01319
KulumbegovAI <43201505+KulumbegovAI@users.noreply.github.com>
2018-09-20 14:48:01 +0200

    Add files via upload

---
+++ KulumbegovAL/Source.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+float main()
+{
+		float left, right;
+		cout << "Enter left value" << "\n";
+		 cin >> left;
+		 cout << "Enter right value" << "\n";
+		 cin >> right;
+	char sign; // char занимает один байт
+	cout << "Enter sign" << "\n";
+	cin >> sign;
+    float result;
+	switch (sign) {
+	case'+':result = left + right;
+		break;
+	case'-':result = left - right;
+		break;
+	case'*':result = left * right;
+		break;
+	case'/':
+		if (right == 0)
+		{
+			cout << "WTF?" << "\n";
+			system("pause");
+			//_getch();
+			break;	
+			
+		}
+		else
+		{
+			result = left / right;
+			break;
+		}
+		system("pause");
+		
+	default:
+		cout << "Wrong sign" << "\n";
+	}
+	if (sign == '+') {
+		result = left + right;
+		cout << "result:" << left + right << "\n";
+		system("pause");
+		//_getch();
+	}
+}
