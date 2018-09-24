0ae699e5ac9fd228dd617d4f7edd7263361ca59d
KulumbegovAI <43201505+KulumbegovAI@users.noreply.github.com>
2018-09-24 23:18:28 +0200

    Update Source.cpp

--- KulumbegovAL/Lab_1/Source.cpp
+++ KulumbegovAL/Lab_1/Source.cpp
@@ -1,29 +1,29 @@
-﻿#include <iostream>
+#include <iostream>
 #include <stdio.h>
-
+#include <conio.h>
 using namespace std;
 int main()
 {
-		float left, right;
-		cout << "Enter left value" << "\n";
-		while (!(cin >> left))
-		{
-			cin.clear();
-			while (cin.get() != '\n') continue;
-			cout << "Enter correct number " << "\n";
-		}
-		 cout << "Enter right value" << "\n";
-		 while (!(cin >> right))
-		 {
-			 cin.clear();
-			 while (cin.get() != '\n') continue;
-			 cout << "Enter correct number " << "\n";
-		 }
+	float left, right;
+	cout << "Enter left value" << "\n";
+	while (!(cin >> left))
+	{
+		cin.clear();
+		while (cin.get() != '\n') continue;
+		cout << "Enter correct number " << "\n";
+	}
+	cout << "Enter right value" << "\n";
+	while (!(cin >> right))
+	{
+		cin.clear();
+		while (cin.get() != '\n') continue;
+		cout << "Enter correct number " << "\n";
+	}
 	char sign; // char занимает один байт
 	cout << "Enter sign" << "\n";
 	cin >> sign;
-    float result;
-	switch (sign) 
+	float result;
+	switch (sign)
 	{
 	case'+':result = left + right;
 		break;
@@ -34,21 +34,20 @@ int main()
 	case'/':
 		if (right == 0)
 		{
-			cout << "WTF?" << "\n";
-			system("pause");
-			break;		
+			cout << "division by 0 is impossible" << "\n";
+			_getch();
+			return 0;
+			break;
 		}
 		else
 		{
 			result = left / right;
 			break;
 		}
-		system("pause");
 	default:
 		cout << "Wrong sign" << "\n";
-		system("pause");
 		return 0;
-	}	
+	}
 	cout << "Result:" << result << "\n";
-	system("pause");
+	_getch();
 }
