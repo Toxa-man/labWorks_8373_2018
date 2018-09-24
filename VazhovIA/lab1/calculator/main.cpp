d52f5f5fc4f33da995736f4e5cab55d7dc19fe2b
l0sted <losted@wants.dicksinhisan.us>
2018-09-24 19:51:21 +0300

    fixed calculator

--- VazhovIA/lab1/calculator/main.cpp
+++ VazhovIA/lab1/calculator/main.cpp
@@ -5,28 +5,32 @@ using namespace std;
 
 int main()
 {
-    float left, right;
+    float left, right, result;
     char sign;
     cin >> left >> sign >> right;
 
     switch (sign)
     {
     case '+':
-        cout << left + right << endl;
+        result = left + right;
         break;
     case '-':
-        cout << left - right << endl;
+        result = left - right;
         break;
     case '*':
-        cout << left * right << endl;
+        result = left * right;
         break;
     case '/':
         if (right == 0) {
             cout << "Division by zero is forbidden" << endl;
             return 8;
         }
-        cout << left / right << endl;
+        result = left / right;
+        break;
+    default:
+        cout << "wrong sign" << endl;
         break;
     }
+    cout << result << endl;
     return 0;
 }
