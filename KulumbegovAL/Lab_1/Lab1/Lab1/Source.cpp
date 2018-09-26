98e27d5b102bf0f7a2746b1bd63502ac2d22b7d6
KulumbegovAI <43201505+KulumbegovAI@users.noreply.github.com>
2018-09-26 15:41:48 +0200

    Add files via upload

---
+++ KulumbegovAL/Lab_1/Lab1/Lab1/Source.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+using namespace std;
+int main()
+{
+	float left, right;
+	int x;
+	x = 0;
+	cout << "Enter left value" << "\n";
+	while (!(cin >> left))
+	{
+		cin.clear();
+		while (cin.get() != '\n') continue;
+		cout << "Enter correct number " << "\n";
+	}
+	cout << "Enter right value" << "\n";
+	while (!(cin >> right))
+	{
+		cin.clear();
+		while (cin.get() != '\n') continue;
+		cout << "Enter correct number " << "\n";
+	}
+	char sign; // char занимает один байт
+	cout << "Enter sign" << "\n";
+	cin >> sign;
+	float result;
+	switch (sign)
+	{
+	case'+':result = left + right;
+		break;
+	case'-':result = left - right;
+		break;
+	case'*':result = left * right;
+		break;
+	case'/':result = left / right;
+		break;
+		if (right == 0)
+		{
+			cout << "division by 0 is impossible" << "\n";
+			_getch();
+			return 0;
+			break;
+		}
+		else
+		{
+			result = left / right;
+			x++;
+			break;
+		}
+	default:
+		cout << "Wrong sign" << "\n";
+		return 0;
+	}
+	if (x == 0) {
+		cout << "Result:" << result << "\n";
+	}
+	_getch();
+}
