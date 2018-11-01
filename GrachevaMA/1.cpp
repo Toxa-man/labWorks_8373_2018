0a727a09fd6952fab2b24c6ca62784a538c05b40
MariaGracheva <43457734+MariaGracheva@users.noreply.github.com>
2018-11-01 23:07:44 +0300

    Add files via upload

---
+++ GrachevaMA/1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <locale>
+
+using namespace std;
+int main()
+{
+	
+	setlocale(LC_ALL, "Rus");
+
+	int left, right;
+	int result;
+	char sign;
+	cout << "Введите число";
+	cin >> left;
+	cout << "Выберите операцию";
+	cin >> sign;
+	cout << "Введите число";
+	cin >> right;
+
+	switch (sign)
+	{
+	case +: cout << "result" << left + right; break;
+	case -: cout << "result" << left - right; break;
+	case / : if (right == 0) { return 2; }
+			 else { cout << "result" << left / right; break; }
+	case *: cout << "result" << left * right; break;
+	default:cout << "Error" << endl; system("pause"); return 1;
+		break;
+	}
+	system("pause"); 
+	return 0;
+	
+
+}
