8904ca95dd7dfe594a64d827e324b4ad7dc8dcde
Cyganov Gleb <gleb.geo@mail.ru>
2018-11-07 16:53:36 +0300

    Create lab4.cpp

---
+++ CyganovGS/lab_4/lab4.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+
+using namespace std;
+
+int step(int ch, int pok){
+	int x=1;
+	for(int h=0; h<pok; h++){
+		x*=ch;
+	}
+	return(x);
+}
+
+int fackt(int arg){
+	int otv=1;
+	for(int f=2; f<=arg; f++){
+		otv*=f;
+	}
+	return otv;
+}
+
+double kvadr_kor(double arg){
+	const double EPS=1E-15;
+	double x=1;
+	for(;;){
+		double xp=(x+arg/x)/2;
+		if (x-xp>=0){
+		 	if(x-xp<EPS){
+				break;
+			}
+		}
+		if (x-xp<0){
+			if(x-xp>-EPS){
+				break;
+			}
+		}
+		x=xp;
+	}
+	return x;
+}
+
+bool my_prime(int arg){
+	for(int i=2; i<arg; i++){
+		if(arg%i==0){
+			return false;
+		}
+	}
+	return true;
+}
+
+int main(){
+	int ch, pok, facktor, kor, pri;
+	cout<<"First we calculate the power of number. Enter a number: ";
+	cin>>ch;
+	cout<<"Enter the power of number: ";
+	cin>>pok;
+	cout<<"Here is the result: "<<step(ch, pok)<<endl;
+	cout<<endl;
+	cout<<"Calculate a facktorial. Print your number: ";
+	cin>>facktor;
+	cout<<facktor<<"! = "<<fackt(facktor)<<endl;
+	cout<<endl;
+	cout<<"From your next number my program will calculate the square root: ";
+	cin>>kor;
+	cout<<"The square root of "<<kor<<" is "<<kvadr_kor(kor)<<endl;
+	cout<<endl;
+	cout<<"Now let's check is your number prime or not? Print your number here: ";
+	cin>>pri;
+	if(my_prime(pri)){
+		cout<<"Your number is prime."<<endl;
+	}else{
+		cout<<"Your number is not prime."<<endl;
+	}
+	cout<<endl;
+	system("pause");
+	return 0;
+}
