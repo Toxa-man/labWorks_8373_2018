bf1437bb2f0fefb92466fe7340e480c0a87397d8
koker57 <43215404+koker57@users.noreply.github.com>
2018-11-09 15:14:20 +0300

    update code

--- KunitsinKA/kolkuluktor/kolkuluktor/kolkuluktor.cpp
+++ KunitsinKA/kolkuluktor/kolkuluktor/kolkuluktor.cpp
@@ -8,46 +8,39 @@ int main()
 	float left, right, result;
 	char sign;
 	setlocale(0, "");
-		cout << "Ââåäèòå ëåâûé àðãóìåíò" << "\n";
-				cin >> left;
-		cout << "Ââåäèòå ïðàâûé àðãóìåíò" << "\n";
-				cin >> right;
-		cout << "Ââåäèòå çíàê âûðàæåíèÿ" << "\n";
-				cin >> sign;
-		
-				while (sign != '+' && sign != '-' && sign != '/' && sign != '*')
-				{
-					cout << "Âû ââåëè íåâåðíûé çíàê, ââåäèòå âåðíûé" << "\n";
-					cin >> sign;
-				}
-					switch (sign)
-					{
-						case'+':
-							result = left + right;
-							break;
-						case'-':
-							result = left - right;
-							break;
-						case'*':
-							result = left*right;
-							break;
-						case'/':
-							if (right == 0)
-							{
-								cout << "Íà íîëü äåëèòü íåëüçÿ, ââåäèòå äðóãîé äåëèòåëü" << "\n";
-								cin >> right;
-								result = left / right;
-								return;
-							}
-							else
-							{
-								result = left / right;
-								break;
-							}
-					}
-				
-
-		cout << left << sign << right << "=" << result;
-		_getch();
+	cout << "Введите левый операнд\n";
+	cin >> left;
+	cout << "Введите правый операнд\n";
+	cin >> right;
+	cout << "Введите знак\n";
+	cin >> sign;
+	while (sign != '+' && sign != '-' && sign != '/' && sign != '*')
+	{
+		cout << "Вы ввели неверный знак - введите новый\n";
+		cin >> sign;
+	}
+	switch (sign)
+	{
+	case'+':
+		result = left + right;
+		break;
+	case'-':
+		result = left - right;
+		break;
+	case'*':
+		result = left*right;
+		break;
+	case'/':
+		while (right == 0)
+		{
+			cout << "Введите новые числа, так как на ноль делить нельзя\n";
+			cin >> left;
+			cin >> right;
+			result = left / right;
+		}			
+		break;
+	}
+	cout << left << sign << right << "=" << result;
+	_getch();
 	return 0;
 }
