cf406c4919bdaffadc78d6999fc280d97b4b3703
Nastyonai <43208046+nastyonai@users.noreply.github.com>
2018-12-19 13:43:43 +0300

    Add files via upload

--- TimofeevaAI/lab_4/Source.cpp
+++ TimofeevaAI/lab_4/Source.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <conio.h>
 #include <stdio.h>
-
 using namespace std;
 
 double Stepen(float number, int stepen)
@@ -37,68 +36,97 @@ double Stepen(float number, int stepen)
 int Fact(int c)
 {
 	if (c < 0)
-		return 0;
-	int f = 1;
-	for (int i = 1; i < c + 1; i++)
+		return -1;
+	else
 	{
-		f *= i;
+	   if (c > 0)
+	   {
+		 int f = 1;
+		 for (int i = 1; i < c + 1; i++)
+		 {
+			 f *= i;
+		 }
+		 return f;
+	   }
+	   else
+	   return 0;
 	}
-	return f;
 }
+
 
 double Koren(double n)
 {
 	float sqrt = 0;
-	if (n <= 0)
-		cout << "Negative number" << endl;
+	if (n < 0)
+		return -1;
 	else
 	{
-		for (int i = 0; i < 10000; i++)
+		if (n > 0)
 		{
-			sqrt = 1 + (n - 1) / (sqrt + 1);
+			for (int i = 0; i < 10000; i++)
+			{
+				sqrt = 1 + (n - 1) / (sqrt + 1);
+			}
+			return sqrt;
 		}
+		else
+			return 0;
 	}
-	return sqrt;
+	
 }
 
 
 int Simple(int m)
 {
-
-	if ((m != 2) && (m != 3) && (m != 5))
-	{
-		if ((m % 2 != 0) && (m % 3 != 0) && (m % 5 != 0))
-			cout << "Simple" << endl;
-		else
-			cout << "Not simple" << endl;
-	}
+	if (m <= 0)
+		return -1;
 	else
 	{
-		cout << "Simple";
+		bool n = true;
+		for (int i = 2; i <= Koren(m); i++)
+		{
+			if (m%i == 0)
+				n = false;
+		}
+		if (n == true)
+			return 1;
+		else
+			return 0;
 	}
-	return 0;
 }
 
 int main()
 {
 	float a, b, h, g, f;
+	
 	cout << "Enter a number: ";
 	cin >> a;
 	cout << "Enter stepen: ";
 	cin >> b;
-	cout << "Result: " << Stepen(a, b) << "\n";
-
+	cout << "Result: " << Stepen(a, b) <<endl;
 
 	cout << "Enter a number to find factorial: ";
 	cin >> h;
+	Fact(h);
+	if (Fact(h) == -1)
+		cout <<"Negative number"<<endl;
+	else
 	cout << "Factorial: " << Fact(h) << endl;
 
 	cout << "Enter a number to extract the square root: ";
 	cin >> g;
-	cout << Koren(g) << endl;
-
+	Koren(g);
+	if (Koren(g) == -1)
+		cout<<"Negative number"<<endl;
+	else
+	cout<<"Sqrt: "<<Koren(g)<<endl;
+	
 	cout << "Enter a number to determine its simplicity: ";
 	cin >> f;
 	Simple(f);
+	if (Simple(f) == 1)
+		cout << "Simple"<<endl;
+	else
+		cout << "Not simple"<<endl;
 	system("pause");
 }
