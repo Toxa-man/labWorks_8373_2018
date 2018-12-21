cc7e62c901d3c636fffdfe16acb064596b63f0a8
TerletskayaJE <43278383+TerletskayaJE@users.noreply.github.com>
2018-12-21 15:21:24 +0300

    Update lab_3.cpp

--- lab_3/lab_3.cpp
+++ lab_3/lab_3.cpp
@@ -1,53 +1,74 @@
-#include <string>
 #include <iostream>
 using namespace std;
 
 int main() {
 	setlocale(0, "");
-	int count = 0, sum = 0, temp = 0;
-	char str1[200];
-	string arr[200], str2;
-	cout << "Aaaaeoa no?ieo: ";
-	cin.getline(str1, 200);
-	for (int i = 0; i < strlen(str1); i++) {
+	int count = 0, sum = 0, temp = 0, counter=0;
+	char str1[100], temp1[1] = {};
+	char words[100][100];
+	for (int i = 0; i < 100; i++) {
+		for (int j = 0; j < 100; j++) {
+			words[i][j] = '\0';
+		}
+	}
+	int k = 0, j = 0;
+	int sums[100];
+	cout << "Введите строку: ";
+	cin.getline(str1, 100);
+	for (int i = 0; i < 100; i++) {
 		if (str1[i] == ' ') {
-			arr[count] = str2;
-			count++;
-			str2 = "";
+			words[j][k] = '\0';
+			k = 0;
+			j++;
+			continue;
 		}
-		else if (i == strlen(str1) - 1) {
-			str2 += str1[i];
-			arr[count] = str2;
-			count++;
+		if (str1[i] == '\0') {
+			words[j][k] = '\0';
+			k = 0;
+			j++;
+			break;
 		}
-		else {
-			str2 += str1[i];
-		}
+		words[j][k] = str1[i];
+		k++;
 	}
-	const int size = 200;
-	int arrdig[size];
-	for (int i = 0; i < count; i++) {
-		for (int z = 0; z < arr[i].size(); z++) {
-			sum += int(arr[i][z]);
+	cout << endl;
+	for (int i = 0; i < j; i++) {
+		counter++;
+		for (int i2 = 0; i2 < 100; i2++) {
+			if (words[i][i2] == '\0') {
+				cout << " -" << sum;
+				sums[i] = sum;
+				sum = 0;
+				break;
+			}
+			cout << words[i][i2];
+			sum = sum + (int)words[i][i2];
 		}
-		arrdig[i] = sum;
-		sum = 0;
+		cout << endl;
 	}
-	for (int i = 0; i < count; i++) {
-		for (int z = 0; z < count - 1; z++) {
-			if (arrdig[z] > arrdig[z + 1]) {
-				temp = arrdig[z];
-				arrdig[z] = arrdig[z + 1];
-				arrdig[z + 1] = temp;
-				str2 = arr[z];
-				arr[z] = arr[z + 1];
-				arr[z + 1] = str2;
+	for (int i = 0; i < counter-1; i++) {
+		for (int z = i; z < counter-1; z++) {
+			if (sums[z] > sums[z + 1] && sums[z] != 0 && sums[z + 1] != 0) {
+				temp = sums[z];
+				sums[z] = sums[z + 1];
+				sums[z + 1] = temp;
+				for (int j = 0; j < 10; j++) {
+					temp1[0] = words[z][j];
+					words[z][j] = words[z + 1][j];
+					words[z + 1][j] = temp1[0];
+				}
 			}
 		}
 	}
-	cout << "Ioni?oe?iaaiiay no?iea: ";
-	for (int i = 0; i < count; i++) {
-		cout << arr[i] << " ";
+	cout << endl;
+	cout << "Отсортированная строка: ";
+	for (int i = 0; i < j; i++) {
+		for (int j = 0; j < 10; j++) {
+			if (words[i][j] != '\0') {
+				cout << words[i][j];
+			}
+			}
+		cout << " ";
 	}
 	cout << endl;
 	return 0;
