d79e533b053d881cd31326a426672a041e1d19b0
Aleksandr Moskofidi <43350388+GramLun@users.noreply.github.com>
2018-12-26 01:02:39 +0300

    Create Source.cpp

---
+++ MoskofidiAA/lab_3/Source.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include "string.h"
+#include "conio.h"
+
+using namespace std;
+
+int main()
+{
+	const int N = 500;
+	char str_in[N];
+	char str_out[N];
+	char str[M][N];
+	int j(0), m(0), l(0), p(0);
+	int len(0);
+	int SumMax(0);
+	int count(1);
+	cout << "Enter string: ";
+	cin.getline(str_in, N);
+	cout << endl;
+	len = strlen(str_in);
+	for (int i = 0; i < len; i++)  
+	{
+		if (str_in[i] == ' ') count += 1;
+	}
+	int *sum = new int[count];
+	int Summa = 0;
+	for (int i = 0; i < count; i++)
+	{
+		while ((str_in[j] != ' ') || (str_in[j] != '\0'))
+		{
+			str[i][m] = str_in[j];
+			sum[i] += (int)str_in[j];
+			j++;
+			m++;
+		}
+		j++;
+		str[i][m] = '\0';
+		m = 0;
+	}
+	for (int t = 0; t < count; t++)
+	{
+		for (int i = 0; i < count; i++)
+		{
+			if (sum[i] > sum[SumMax])
+			{
+				SumMax = i;
+			}
+		}
+		while (str[SumMax][p] != '\0')
+		{
+			str_out[l] = str[SumMax][p];
+			l++;
+			p++;
+		}
+		str_out[l] = ' ';
+		l++;
+		sum[SumMax] = 0;
+		p = 0;
+	}
+	for (int i = 0; i < len; i++)
+	{
+		cout << str_out[i];
+	}
+	_getch();
+	return 0;
+}
