c655bdb3e1af2df9714067c5e6e31e3d1d8910a4
TerletskayaJE <43278383+TerletskayaJE@users.noreply.github.com>
2018-12-25 10:19:26 +0300

    Update lab_3.cpp

--- lab_3/lab_3.cpp
+++ lab_3/lab_3.cpp
@@ -4,18 +4,19 @@ using namespace std;
 int main() {
 	setlocale(0, "");
 	int count = 0, sum = 0, temp = 0, counter = 0;
-	char str1[100], temp1[1] = {};
-	char words[100][100];
-	for (int i = 0; i < 100; i++) {
-		for (int j = 0; j < 100; j++) {
+	int const size = 100;
+	char str1[size], temp1[1] = {};
+	char words[size][size];
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
 			words[i][j] = '\0';
 		}
 	}
 	int k = 0, j = 0;
-	int sums[100];
+	int sums[size];
 	cout << "Введите строку: ";
 	cin.getline(str1, 100);
-	for (int i = 0; i < 100; i++) {
+	for (int i = 0; i < size; i++) {
 		if (str1[i] == ' ') {
 			words[j][k] = '\0';
 			k = 0;
@@ -34,7 +35,7 @@ int main() {
 	cout << endl;
 	for (int i = 0; i < j; i++) {
 		counter++;
-		for (int i2 = 0; i2 < 100; i2++) {
+		for (int i2 = 0; i2 < size; i2++) {
 			if (words[i][i2] == '\0') {
 				cout << " -" << sum;
 				sums[i] = sum;
@@ -46,16 +47,16 @@ int main() {
 		}
 		cout << endl;
 	}
-	for (int i = 0; i < counter-1; i++) {
-		for (int z = i; z < counter-1; z++) {
-			if (sums[z] > sums[z + 1] && sums[z] != 0 && sums[z + 1] != 0) {
-				temp = sums[z];
-				sums[z] = sums[z + 1];
-				sums[z + 1] = temp;
-				for (int j = 0; j < 10; j++) {
-					temp1[0] = words[z][j];
-					words[z][j] = words[z + 1][j];
-					words[z + 1][j] = temp1[0];
+	for (int i = 0; i < counter - 1; i++) {
+		for (int z = i + 1; z < counter; z++) {
+			if (sums[i] > sums[z] && sums[i] != 0 && sums[z] != 0) {
+				temp = sums[i];
+				sums[i] = sums[z];
+				sums[z] = temp;
+				for (int j = 0; j < 15; j++) {
+					temp1[0] = words[i][j];
+					words[i][j] = words[z][j];
+					words[z][j] = temp1[0];
 				}
 			}
 		}
@@ -63,11 +64,11 @@ int main() {
 	cout << endl;
 	cout << "Отсортированная строка: ";
 	for (int i = 0; i < j; i++) {
-		for (int j = 0; j < 10; j++) {
+		for (int j = 0; j < 15; j++) {
 			if (words[i][j] != '\0') {
 				cout << words[i][j];
 			}
-			}
+		}
 		cout << " ";
 	}
 	cout << endl;
