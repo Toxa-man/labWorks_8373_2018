5fd0fa0460fd4c71a893115600ad2aacecdff31a
Nastyonai <43208046+nastyonai@users.noreply.github.com>
2018-12-25 22:45:55 +0300

    Add files via upload

--- TimofeevaAI/lab_3/Source.cpp
+++ TimofeevaAI/lab_3/Source.cpp
@@ -1,33 +1,57 @@
-#include <string> 
-#include <vector> 
-#include <sstream> 
-#include <iostream> 
+#include<iostream>
+#include<stdio.h>
+#include<conio.h>
 using namespace std;
 
 int main() {
-	string s, w, c;
-
-	cout << "Enter words: " << endl;
-	getline(cin, s);
-
-	istringstream iss(s);
-	vector<string> v;
+	const int size = 100;
+	int size_of_str;
+	int wordlen, word = 0, n = 0, p1 = 0,p2 = 0;
+	char str[size];
+	char sorted_str[size][size];
+	int sumofcodes[size];
 	
-	while (iss >> w) 
-		v.push_back(w);
-
-	for (int i = 0; i < v.size() - 1; i++) {
-		for (int j = 0; j < v.size() - i - 1; j++) {
-			if (v[j].size() > v[j + 1].size()) {
-				c = v[j];
-				v[j] = v[j + 1];
-				v[j + 1] = c;
-			}
+	cout << "Enter string: ";
+	cin.getline(str, size);
+	size_of_str = strlen(str);
+	
+	while (n < size_of_str) {
+		wordlen = 0;
+		sumofcodes[word] = 0;
+		while ((str[n] >= 65 && str[n] <= 90) || (str[n] >= 97 && str[n] <= 122)) {
+			sorted_str[word][wordlen] = str[n];
+			sumofcodes[word] += (int)(str[n]);
+			wordlen++;
+			n++;
 		}
+		sorted_str[word][wordlen] = '\0';
+		word++;
+		n++;
 	}
 
-	cout << "Sorted string: " << endl;
-	for (int c = 0; c < v.size(); ++c) cout << v[c] << " ";
+	int i = 1, j = 0;
+	while (i < word) {
+		j = i;
+		while (sumofcodes[j] < sumofcodes[j - 1]) {
+			p1 = sumofcodes[j];
+			sumofcodes[j] = sumofcodes[j - 1];
+			sumofcodes[j - 1] = p1;
+
+			for (int w = 0; w < size; w++) {
+				 p2 = sorted_str[j][w];
+				sorted_str[j][w] = sorted_str[j - 1][w];
+				sorted_str[j - 1][w] = p2;
+			}
+			j--;
+		}
+		i++;
+	}
+
+	cout << "Result: ";
+	for (int x = 0; x < word; x++) {
+		cout << sorted_str[x] << " ";
+	}
+	cout << endl;
 	system("pause");
 	return 0;
 }
