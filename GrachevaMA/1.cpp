2ccb66081a7ba367c01a1ee88d6590c92f3dc811
MariaGracheva <43457734+MariaGracheva@users.noreply.github.com>
2019-02-01 02:31:22 +0300

    Update 1.cpp

--- GrachevaMA/1.cpp
+++ GrachevaMA/1.cpp
@@ -4,31 +4,35 @@
 using namespace std;
 int main()
 {
-	
+
 	setlocale(LC_ALL, "Rus");
 
-	int left, right;
-	int result;
+	double left, right;
 	char sign;
-	cout << "Введите число";
+	cout << "Введите число ";
 	cin >> left;
-	cout << "Выберите операцию";
+	cout << "Выберите операцию ";
 	cin >> sign;
-	cout << "Введите число";
+	cout << "Введите число ";
 	cin >> right;
 
 	switch (sign)
 	{
-	case +: cout << "result" << left + right; break;
-	case -: cout << "result" << left - right; break;
-	case / : if (right == 0) { return 2; }
-			 else { cout << "result" << left / right; break; }
-	case *: cout << "result" << left * right; break;
-	default:cout << "Error" << endl; system("pause"); return 1;
+	case '+': cout << "result " << left + right; break;
+	case '-': cout << "result " << left - right; break;
+	case '/':
+
+		if (right == 0) { return 2; }
+		cout << "result" << left / right;
 		break;
+
+	case '*': cout << "result" << left * right; break;
+	default:cout << "Error" << endl; system("pause"); return 1;
 	}
-	system("pause"); 
+	system("pause");
 	return 0;
-	
+
+
+}
 
 }
